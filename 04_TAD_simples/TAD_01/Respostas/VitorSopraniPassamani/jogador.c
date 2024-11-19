663ec240391bb2fc9c0eb5b5cd1632237b239634
vitorsoprani <vitor.spassamani@gmail.com>
2024-11-19 14:50:45 -0300

    inicio de resolução tad_01

---
+++ 04_TAD_simples/TAD_01/Respostas/VitorSopraniPassamani/jogador.c
@@ -0,0 +1,83 @@
+// #include "tabuleiro.h"
+
+
+// #define ID_JOGADOR_1 1
+// #define ID_JOGADOR_2 2
+
+
+// typedef struct{
+//     int id;
+// } tJogador;
+
+#include "jogador.h"
+
+/**
+ * Cria um jogador com o id passado como parâmetro e retorna o jogador criado.
+ * 
+ * @param idJogador o id do jogador (1 ou 2).
+ * 
+ * @return tJogador o jogador criado.
+ */
+tJogador CriaJogador(int idJogador) {
+    tJogador jogador;
+    jogador.id = idJogador;
+
+    return jogador;
+}
+
+
+/**
+ * Recebe um jogador e um tabuleiro e retorna o tabuleiro com a jogada do jogador.
+ * 
+ * @param jogador o jogador que fará a jogada.
+ * @param tabuleiro o tabuleiro atual.
+ * 
+ * @return o tabuleiro atualizado com a jogada do jogador.
+ */
+tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro);
+
+
+/**
+ * Recebe um jogador e um tabuleiro e retorna 1 se o jogador venceu e 0 caso contrário.
+ * 
+ * @param jogador o jogador a ser verificado.
+ * @param tabuleiro o tabuleiro atual.
+ * 
+ * @return 1 se o jogador venceu, 0 caso contrário.
+ */
+int VenceuJogador(tJogador jogador, tTabuleiro tabuleiro) {
+    
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        int ganhou = 1;
+        
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            ganhou = ganhou && EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, j, i, jogador.id);
+        }
+
+        if (ganhou) return 1;
+    }
+
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        int ganhou = 1;
+
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            ganhou = ganhou && EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, i, j, jogador.id);
+        }
+
+        if (ganhou) return 1;
+    }
+
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, jogador.id) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, jogador.id) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, jogador.id)) {
+            return 1;
+        }
+
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, jogador.id) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, jogador.id) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, jogador.id)) {
+            return 1;
+        }
+
+    return 0;
+}
---
+++ 04_TAD_simples/TAD_01/Respostas/VitorSopraniPassamani/tabuleiro.c
@@ -0,0 +1,125 @@
+#include "tabuleiro.h"
+
+/**
+ * Cria um tabuleiro e retorna o tabuleiro criado.
+ * 
+ * @return o tabuleiro criado.
+ */
+tTabuleiro CriaTabuleiro() {
+    tTabuleiro tabuleiro;
+
+    tabuleiro.pecaVazio = '-';
+    tabuleiro.peca1 = 'x';
+    tabuleiro.peca2 = '0';
+
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            tabuleiro.posicoes[i][j] = tabuleiro.pecaVazio;
+        }
+    }
+
+    return tabuleiro;
+}
+
+
+/**
+ * Marca uma posição do tabuleiro com a peça do jogador e retorna o tabuleiro.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ * @param peca a peça do jogador (1 ou 2).
+ * @param x a coordenada x da posição a ser marcada.
+ * @param y a coordenada y da posição a ser marcada.
+ * 
+ * @return o tabuleiro atualizado.
+ */
+tTabuleiro MarcaPosicaoTabuleiro(tTabuleiro tabuleiro, int peca, int x, int y) {
+    if (peca == PECA_1) {
+        tabuleiro.posicoes[y][x] = tabuleiro.peca1;
+    } else if (peca == PECA_2) {
+        tabuleiro.posicoes[y][x] = tabuleiro.peca2;
+    }
+
+    return tabuleiro;
+}
+
+
+/**
+ * Verifica se há alguma posição livre no tabuleiro.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ * 
+ * @return 1 se há posição livre, 0 caso contrário.
+ */
+int TemPosicaoLivreTabuleiro(tTabuleiro tabuleiro) {
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            if(EstaLivrePosicaoTabuleiro(tabuleiro, j, i)) return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+/**
+ * Verifica se a posição do tabuleiro está marcada com a peça do jogador.
+ * 
+ * @param tabuleiro - o tabuleiro atual.
+ * @param x a coordenada x da posição a ser verificada.
+ * @param y a coordenada y da posição a ser verificada.
+ * @param peca a peça do jogador (1 ou 2).
+ * 
+ * @return 1 se a posição está marcada com a peça do jogador, 0 caso contrário.
+ */
+int EstaMarcadaPosicaoPecaTabuleiro(tTabuleiro tabuleiro, int x, int y, int peca) {
+    if (peca == PECA_1) {
+        if (tabuleiro.posicoes[y][x] == tabuleiro.peca1) return 1;
+    } else if (peca == PECA_2) {
+        if (tabuleiro.posicoes[y][x] == tabuleiro.peca2) return 1;
+    }
+
+    return 0;
+}
+
+
+/**
+ * Verifica se a posição do tabuleiro está livre.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ * @param x a coordenada x da posição a ser verificada.
+ * @param y a coordenada y da posição a ser verificada.
+ * 
+ * @return 1 se a posição está livre, 0 caso contrário.
+ */
+int EstaLivrePosicaoTabuleiro(tTabuleiro tabuleiro, int x, int y) {
+    return (tabuleiro.posicoes[y][x] == tabuleiro.pecaVazio);
+}
+
+/**
+ * Verifica se a posição do tabuleiro é válida.
+ * 
+ * @param x a coordenada x da posição a ser verificada.
+ * @param y a coordenada y da posição a ser verificada.
+ * @return 1 se a posição é válida, 0 caso contrário.
+*/
+int EhPosicaoValidaTabuleiro(int x, int y) {
+    int EhValido = 1;
+    EhValido = EhValido && (x <= 2 && x >= 0);
+    EhValido = EhValido && (y <= 2 && y >= 0);
+    return EhValido;
+}
+
+
+/**
+ * Verifica se o tabuleiro está cheio.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ */
+void ImprimeTabuleiro(tTabuleiro tabuleiro) {
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        printf("\t");
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            printf("%c", tabuleiro.posicoes[i][j]);
+        }
+    }
+}
