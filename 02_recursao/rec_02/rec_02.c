a3704534033b75c1766f7873cbe560ed9324b1d8
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-11-24 17:45:35 -0300

    resolução rec_02

---
+++ 02_recursao/rec_02/rec_02.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Função recursiva que retorna a soma dos elementos pares.
+int SomaElementosPares(int* vet, int numElementos);
+
+void LeVetorIntr(int* vet, int numElementos);
+
+int main() {
+    int qtdVetores;
+    scanf("%d", &qtdVetores);
+
+    for (int i = 0; i < qtdVetores; i++) {
+        int numElementos;
+        scanf("%d", &numElementos);
+
+        int vet[numElementos];
+        LeVetorIntr(vet, numElementos);
+
+        printf("%d\n", SomaElementosPares(vet, numElementos));
+    }
+
+    return 0;
+}
+
+int SomaElementosPares(int* vet, int numElementos) {
+    int soma = 0;
+    int idx = numElementos - 1;
+
+    if (idx < 0) return 0;
+
+    if (vet[idx] % 2 == 0) { //É par;
+        return vet[idx] + SomaElementosPares(vet, idx);
+    } else {
+        return SomaElementosPares(vet, idx);
+    }
+}
+
+void LeVetorIntr(int* vet, int numElementos) {
+    for (int i = 0; i < numElementos; i++) {
+        scanf("%d", &vet[i]);
+    }
+}
